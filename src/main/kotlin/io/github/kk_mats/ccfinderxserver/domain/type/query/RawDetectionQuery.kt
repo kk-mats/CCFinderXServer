ac1f3a49edeb5d31f1c61e74adec4a0fb50b234b
kk-mats <k.mats.kk@gmail.com>
2019-11-10 16:54:07 +0900

    feat: Add new DetectionQuery type

---
+++ src/main/kotlin/io/github/kk_mats/ccfinderxserver/domain/type/query/RawDetectionQuery.kt
@@ -0,0 +1,14 @@
+package io.github.kk_mats.ccfinderxserver.domain.type.query
+
+import io.github.kk_mats.ccfinderxserver.domain.type.Label
+
+data class TargetPath(val relative: String, val absolute: String)
+
+data class RawDetectionQuery(val target: TargetPath, val output: String, val parameters: HashMap<String, String>)
+
+data class DetectionQuery(
+        val target: TargetPath,
+        val output: String,
+        val version: Label,
+        val parameters: HashMap<Label, String>
+)
