5a0198acdcfd55c1f432f32f8cbc0e89909cdfb0
kk-mats <k.mats.kk@gmail.com>
2019-11-10 16:44:47 +0900

    feat: Set up Spring Boot application

---
+++ src/test/kotlin/io/github/kk_mats/ccfinderxserver/CCFinderXServerApplicationTests.kt
@@ -0,0 +1,12 @@
+package io.github.kk_mats.ccfinderxserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CCFinderXServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
